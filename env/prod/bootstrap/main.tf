7e3ff780c730e7fe2974f9ae3a2feea6d879ef97
jiyong <jiyeong627@gmail.com>
2026-01-29 07:45:02 +0900

    fix: add prevent_destroy

---
+++ env/prod/bootstrap/main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "ca-central-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "finance-app-tfstate-prod"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "finance-app-terraform-lock-prod"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
